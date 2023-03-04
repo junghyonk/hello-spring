3be68b872027549218979384a9b43ef0e2ab6c34
김정현 <junghyonk@gmail.com>
2023-03-04 17:12:26 +0900

    test

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Service
 public class MemberService {
-    //test
+    //test2
     private final MemberRepository memberRepository ;
     @Autowired
     public MemberService(MemberRepository memberRepository) {
